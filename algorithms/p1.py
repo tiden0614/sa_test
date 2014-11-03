6e8491fdda41863575e3414428a1f7ba38eecb2a
Daryl Zhang <tiden111@gmail.com>
2014-11-04 10:18:04 +1030

    p1 solved

--- algorithms/p1.py
+++ algorithms/p1.py
@@ -9,10 +9,13 @@ multiples = []
 
 def get_all_multiples():
     for f in factors:
-        for i in xrange(0, N + 1):
+        for i in xrange(1, N + 1):
             if i % f == 0:
                 multiples.append(i)
 
 
-get_all_multiples()
-print multiples
+if '__main__' == __name__:
+    if len(sys.argv) > 1:
+        N = sys.argv[1]
+    get_all_multiples()
+    print sum(multiples)
