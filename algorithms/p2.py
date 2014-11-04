cbd61f1b50514136c7b2355b6512f2da2a69b87f
Daryl Zhang <tiden111@gmail.com>
2014-11-04 10:31:30 +1030

    p2 solved

--- algorithms/p2.py
+++ algorithms/p2.py
@@ -3,21 +3,26 @@
 
 import sys
 
-N = 100
-prime_marks = [0 for i in xrange(0, N + 1)]
+n = 1500000
 
-def mark_primes():
+def mark_primes(N):
+    prime_marks = [0 for i in xrange(0, N + 1)]
     for i in xrange(2, N + 1):
         if prime_marks[i] == 0:
             for j in xrange(i + i, N + 1, i):
                 prime_marks[j] = 1
+    return prime_marks
 
 
-def print_primes():
+def get_primes(prime_marks):
+    primes = []
     for i, m in enumerate(prime_marks):
         if m == 0 and i > 1:
-            print i,
-    print ''
+            primes.append(i)
+    return primes
 
-mark_primes()
-print_primes()
+
+if '__main__' == __name__:
+    if len(sys.argv) > 1:
+        n = int(sys.argv)
+    print sum(get_primes(mark_primes(n)))
