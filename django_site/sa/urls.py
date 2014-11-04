edcf8db1df257030a632d39a113ec7d3cf69ba16
Daryl Zhang <tiden111@gmail.com>
2014-11-04 12:22:54 +1030

    render index view

---
+++ django_site/sa/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from sa import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_site.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index),
+)
--- django_site/sa/views.py
+++ django_site/sa/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import django.template
 
 # Create your views here.
+def index(request):
+    context = { 'message': 'test' }
+    return render(request, 'sa/index.html', context)
